09ee46c84d8b36dcc551915cb211abb2e33b045d
WhkYes <1055363843@qq.com>
2021-02-02 21:34:07 +0800

    登录和退出功能完成

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 // 项目入口文件
 import Vue from 'vue'
-import App from './App.vue'
+import App from './views/App.vue'
 import router from './router'
 import './plugins/element.js'
 import './assets/css/global.css'
